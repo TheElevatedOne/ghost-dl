7a95dd45fdb9392b2d33a7d98663b4be21326ceb
Adam Mlady <admin@elevated.ovh>
2025-08-29 23:48:08 +0200

    It's working

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import enum
 import sys
 import argparse
 import requests
+import os
 import os.path as op
 from bs4 import BeautifulSoup as bs
 
@@ -52,11 +52,15 @@ def arg_parser() -> argparse.Namespace:
 
 class GhostDL:
     def __init__(
-        self, url: str, dir: str, default: bool, quiet: bool, verbose: bool
+        self, url: str, dir, default: bool, quiet: bool, verbose: bool
     ) -> None:
         self.log = "\033[1;33m[LOG]\033[0m "
         self.error = "\033[31m[ERROR]\033[0m "
-        self.input = "\033[1;34m[INPUT]\033[m "
+        self.input = "\033[1;34m[INPUT]\033[0m "
+        self.dl = "\033[38;2;255;0;255m[DL]\033[0m "
+        self.done = "\033[38;2;0;255;40m[DONE]\033[0m "
+        self.final = "\033[38;2;0;242;255m[FINAL]\033[0m "
+
         if "downloads.khinsider.com" not in url:
             print(f"{self.log}  URL not matching khinsider")
             print(f"{self.error}Invalid URL")
@@ -64,9 +68,17 @@ class GhostDL:
         else:
             self.url = url
         if dir is not None:
-            g = 0
+            print(f"{self.log}Checking custom directory validity")
+            if op.isdir(op.abspath(dir)):
+                print(f"{self.log}Custom directory valid")
+                self.dir = op.abspath(dir)
+            else:
+                print(f"{self.log}Custom directory invalid")
+                print(f"{self.log}Selecting current directory")
+                self.dir = os.getcwd()
         else:
-            g = 1
+            print(f"{self.log}Selecting current directory")
+            self.dir = os.getcwd()
         self.default = default
         if quiet and verbose:
             print(f"{self.error}Quiet and Verbose cannot be used at the same time")
@@ -133,30 +145,34 @@ class GhostDL:
         ]
         soup_fulltitle = f"{soup_year} - {soup_title} ({soup_type})"
         soup_albumart = [
-            s.find("img")["src"]
+            s.find("a")["href"]
             for s in soup.find_all("div", attrs={"class": "albumImage"})
         ]
 
+        soup_filetype = soup_file[-1]
         if not self.default:
             print()
             self.filetype_input(soup_file)
             while True:
                 try:
-                    select = input(": ")
+                    select = input(":")
                     soup_filetype = int(select)
                     if (len(soup_file) - 1) < soup_filetype:
                         raise ValueError(
                             f"{soup_filetype} is larger than the list length"
                         )
                     else:
+                        soup_filetype = soup_file[soup_filetype]
                         print()
                         break
-                except Exception as err:
+                except Exception:
                     print(f"{self.error}Not a valid input")
                     print()
                     self.filetype_input(soup_file)
 
-        print(f"{self.log}Loading Songs") if not self.quiet else self.do_nothing()
+        print(
+            f"{self.log}Loading Songs (This will take a while)"
+        ) if not self.quiet else self.do_nothing()
         soup_songlist = soup.find("table", attrs={"id": "songlist"}).find_all("tr")
         soup_songs = []
         for i in soup_songlist:
@@ -171,7 +187,71 @@ class GhostDL:
             title = key.text
             url = f"https://downloads.khinsider.com{key['href']}"
 
-            soup_songs.append([disk, track, title, url])
+            song_soup = bs(requests.get(url, headers=headers).content, "html.parser")
+            song_links = [
+                s["href"] for s in song_soup.find_all("a") if "vgmsite" in str(s)
+            ]
+            song_link = [s for s in song_links if soup_filetype in s][0]
+
+            soup_songs.append([disk, track, title, song_link, url])
+
+        print(f"{self.log}Preparing Download Folder")
+        os.mkdir(op.join(self.dir, soup_fulltitle)) if not op.isdir(
+            op.join(self.dir, soup_fulltitle)
+        ) else self.do_nothing()
+        print(f"{self.log}Starting Download of Album Art/s")
+        for i, url in enumerate(soup_albumart):
+            with open(op.join(self.dir, soup_fulltitle, f"cover_{i}.jpg"), "wb") as f:
+                response = requests.get(url, stream=True)
+                total_length = response.headers.get("content-length")
+
+                if total_length is None:  # no content length header
+                    f.write(response.content)
+                else:
+                    dl = 0
+                    total_length = int(total_length)
+                    for data in response.iter_content(chunk_size=4096):
+                        dl += len(data)
+                        f.write(data)
+                        done = int(50 * dl / total_length)
+                        sys.stdout.write(
+                            f" {self.dl}Downloading Cover #{i:02}\r[%s%s]"
+                            % ("=" * done, " " * (50 - done))
+                        )
+                        sys.stdout.flush()
+            print(f" {self.done}Cover #{i:02} Downloaded")
+        print()
+        print(f"{self.log}Starting Download of Songs")
+        for i in soup_songs:
+            with open(
+                op.join(
+                    self.dir, soup_fulltitle, f"CD{i[0]} {i[1]}. {i[2]}.{soup_filetype}"
+                ),
+                "wb",
+            ) as f:
+                response = requests.get(i[3], stream=True)
+                total_length = response.headers.get("content-length")
+
+                if total_length is None:  # no content length header
+                    f.write(response.content)
+                else:
+                    dl = 0
+                    total_length = int(total_length)
+                    for data in response.iter_content(chunk_size=4096):
+                        dl += len(data)
+                        f.write(data)
+                        done = int(50 * dl / total_length)
+                        sys.stdout.write(
+                            f" {self.dl}Downloading Song CD{i[0]} {i[1]}. {i[2]}.{soup_filetype}\r[%s%s]"
+                            % ("=" * done, " " * (50 - done))
+                        )
+                        sys.stdout.flush()
+            print(
+                f" {self.done}Song CD{i[0]} {i[1]}. {i[2]}.{soup_filetype} Downloaded"
+            )
+        print()
+        print(f"{self.final}Album Download Complete")
+        print()
 
 
 if __name__ == "__main__":
@@ -187,5 +267,5 @@ if __name__ == "__main__":
         dl.scraper()
     except KeyboardInterrupt:
         print()
-        print("\033[1;32m[INTERRUPT]\033[0m Process Stopped")
+        print("\033[38;2;229;80;26m[INTERRUPT]\033[0m Process Stopped")
         sys.exit(1)
